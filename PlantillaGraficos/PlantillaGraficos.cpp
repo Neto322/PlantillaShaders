2302f887fd3eed52c5884fe7847b4a15656399a1
alumno <alumno@E302>
2020-03-05 07:49:07 -0700

    Implemetando shaders en el dibujado

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -11,8 +11,49 @@
 
 #include <iostream>
 
+#include "Shader.h"
+#include "Vertice.h"
+
+
+#include <vector>
+
 using namespace std;
 
+//Cada elemento que queramos renderear necesita un vertex array y un buffer
+vector<Vertice> triangulo;
+GLuint vertexArrayTriangulo;
+GLuint bufferTrianguloID;
+
+
+//Instancia del shader
+Shader* shader;
+
+//Identificadores para mapeo de atributos de entrada del vertex shader
+GLuint posicionID;
+GLuint colorID;
+
+void inicializarTriangulo()
+{
+	Vertice v1 = {
+		vec3(0.0f,0.3f,0.0f),
+		vec4(0.08f,0.1f,0.0f,1.0f)
+	};
+
+	Vertice v2 = {
+		vec3(-0.3f,-0.3f,0.0f),
+		vec4(0.8f,0.1f,0.0f,1.0f)
+	};
+
+	Vertice v3 = {
+		vec3(0.3f,-0.3f,0.0f),
+		vec4(0.8f,0.1f,0.0f,1.0f)
+	};
+
+	triangulo.push_back(v1);
+	triangulo.push_back(v2);
+	triangulo.push_back(v3);
+
+}
 
 void dibujar() {
 
@@ -60,6 +101,13 @@ int main()
 
 	const GLubyte* versionGL = glGetString(GL_VERSION);
 	cout << "Version Opengl: " << versionGL;
+
+	inicializarTriangulo();
+
+	const char* rutaVertexShader = "VertexShader.shader";
+
+	const char* rutaFragmentShader = "FragmentShader.shader";
+	shader = new Shader(rutaVertexShader, rutaFragmentShader);
 
 	//Ciclo de dibujo (Draw Loop)
 	while (!glfwWindowShouldClose(window))
