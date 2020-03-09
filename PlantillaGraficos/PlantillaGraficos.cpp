b4516c5f8cff22ebf3b7e450d4f18ae2de828376
alumno <alumno@E302>
2020-03-09 07:52:39 -0700

    Se completo el shader pero tiene errores :_(

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -56,7 +56,16 @@ void inicializarTriangulo()
 }
 
 void dibujar() {
-
+	//Elegir shader
+	shader->enlazar();
+	//Elegir el vertex array
+	glBindVertexArray(vertexArrayTrianguloID);
+	//Dibujar
+	glDrawArrays(GL_TRIANGLES, 0, triangulo.size());
+	//Soltar vertex array
+	glBindVertexArray(0);
+	//Desenlazar shader
+	shader->desenlazar();
 }
 
 int main()
@@ -135,9 +144,11 @@ int main()
 
 	//Especificar a OpenGL como comunicarse
 	glVertexAttribPointer(posicionID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
+	glVertexAttribPointer(colorID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertice), (void*)sizeof(vec3));
 
-
-
+	//Soltar el vertex array y el buffer
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER,0);
 
 	//Ciclo de dibujo (Draw Loop)
 	while (!glfwWindowShouldClose(window))
